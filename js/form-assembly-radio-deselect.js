2df8958c77a8e26a979c50ecfe308cb94ad63340
context-dependent <tmcmanus@BPA.LOCAL>
2019-06-06 14:17:41 -0400

    carry_forward done

--- js/form-assembly-radio-deselect.js
+++ js/form-assembly-radio-deselect.js
@@ -21,10 +21,9 @@ function make_radio_group_exclusive(radio_group_id) {
 	// Define a function that adds an event listener to each radio button that triggers 
 	// unselect_all_radios_but_one whenever they are clicked
 	function add_click_event_listener(arradios) {
-
-		arradios.map((d, i) => {
+		arradios.map((d, i, r) => {
 			d.onclick = () => {
-				unselect_all_radios_but_one(arradios, i);
+				unselect_all_radios_but_one(r, i);
 			}
 		});
 	}
@@ -33,3 +32,90 @@ function make_radio_group_exclusive(radio_group_id) {
 	add_click_event_listener(target_radio_array);
 
 }
+
+
+function update_switch_array(checkbox_array) {
+
+	let switch_array = checkbox_array.map((d, i) => {return d.checked});
+
+	console.log(switch_array);
+
+	return switch_array;
+};
+
+function show_hide_radios(radio_group_id, radio_field_array, switch_array) {
+
+	let n_selected = switch_array.reduce((a, b) => a + b, 0);
+	let radio_group = document.getElementById(radio_group_id);
+
+	if(n_selected > 1) {
+
+		radio_group.classList.remove("offstate");
+
+		radio_field_array.map((d, i, r) => {
+			if(!switch_array[i]) {
+				d.classList.add("offstate")
+			} else {
+				d.classList.remove("offstate")
+			}
+		}); 
+
+	} else {
+		radio_group.classList.add("offstate");
+	}
+};
+
+function get_radio_field_array(radio_group_id) {
+
+	let radio_field_nodes = document
+		.getElementById(radio_group_id)
+		.getElementsByClassName("oneField");
+
+	let radio_field_array = Array.from(radio_field_nodes);
+
+	return radio_field_array;
+
+};
+
+function get_checkbox_input_array(checkbox_group_id) {
+
+	let target_check_nodes = document
+		.querySelectorAll("#" + checkbox_group_id + " input[type='checkbox']");
+
+	let target_check_array = Array.from(target_check_nodes);
+
+	return target_check_array;
+
+
+};
+
+
+function carry_forward(source_id, target_id) {
+
+
+	let switch_array = [];
+	let checkbox_input_array = get_checkbox_input_array(source_id);
+	let radio_field_array = get_radio_field_array(target_id);
+	let radio_group = document.getElementById(target_id);
+
+	radio_group.classList.add("offstate");
+
+	radio_field_array.map((d, i) => {
+		d.classList.add("offstate");
+	});
+
+	function add_click_event_listener(checkbox_input_array, radio_field_array) {
+		checkbox_input_array.map((d, i, r) => {
+
+			d.onclick = () => {
+				switch_array = update_switch_array(r);
+				show_hide_radios(target_id, radio_field_array, switch_array);
+			};
+
+		});
+	};
+
+	add_click_event_listener(checkbox_input_array, radio_field_array);
+
+
+};
